5bc70603e0b739d095ce36d921f027dbad6b6bb2
Jujusko <68372976+Jujusko@users.noreply.github.com>
2021-10-24 23:37:08 +0500

    Add files via upload

---
+++ ThirdTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEducation
+{
+    class ThirdTask
+    {
+        public static void DivAndMod()
+        {
+            int a, b;
+            int mod, div;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            mod = a % b;
+            div = a / b;
+            Console.WriteLine("res by mod = " + mod);
+            Console.WriteLine("res by div = " + div);
+        }
+    }
+}
